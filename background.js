0a6660d21cea6a8f0d0c49c51d2567b2e9f80428
0slot <0slot.trade@gmail.com>
2026-03-26 15:12:51 +0800

    Update background.js

--- background.js
+++ background.js
@@ -5,25 +5,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        // 检查返回数据是否为数组且有内容
         const isBundle = Array.isArray(data) && data.length > 0 && data[0].bundle_id;
         
         if (isBundle) {
           const bundleId = data[0].bundle_id;
           const bundleUrl = `https://explorer.jito.wtf/bundle/${bundleId}`;
           
-          // 获取 bundle 详细信息
           fetch(`https://bundles.jito.wtf/api/v1/bundles/bundle/${bundleId}`)
             .then(response => response.json())
             .then(bundleData => {
               if (Array.isArray(bundleData) && bundleData.length > 0) {
                 const landedTipLamports = bundleData[0].landedTipLamports;
-                // 将 lamports 转换为 SOL (1 SOL = 1,000,000,000 lamports)
+                
                 const solAmount = landedTipLamports / 1000000000;
-                // 格式化 SOL 数量
+                
                 const formattedSol = Number(solAmount).toString();    
                 const validatorTip = formattedSol   
-                // 发送结果给 content.js                
+                
                 sendResponse({
                   success: true,
                   isBundle,
